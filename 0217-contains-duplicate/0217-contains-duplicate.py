52bcffc719095c62fd79cbb17f09cb003e6cf8e4
paulankita924-dev <paulankita924@gmail.com>
2026-08-13 16:21:22 +0530

    Update 0217-contains-duplicate.py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         seen = set()
         for num in nums:
             if num in seen:
